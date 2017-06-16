a806b9fe4b76c87e0ad0a84a2f8c413c17fbaf46
Victor <vihto8@gmail.com>
2017-06-16 06:59:59 -0300

    First commit

---
+++ The_Reaper/Reaper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robocode;
+
+namespace The_Reaper
+{
+    public class Reaper : Robot
+    {
+        /// <summary>
+        /// Robo anda em movimentos triangulares
+        /// </summary>
+        public override void Run()
+        {
+            while(true)
+            {
+                Ahead(500);
+                TurnRight(135);
+                TurnGunRight(720);
+                Ahead(500);
+                TurnRight(135);
+                TurnGunRight(720);
+            }
+        }
+
+        /// <summary>
+        /// Quando um robo é detectado 
+        /// </summary>
+        /// <param name="evnt"></param>
+        public override void OnScannedRobot(ScannedRobotEvent evnt)
+        {
+            if (evnt.Distance < 300) //se detectado um 300 pixes distante
+            {
+                Fire(3);
+                TurnLeft(evnt.Bearing);
+                Fire(3);
+                TurnLeft(90); //atira e se distancia para distancia contrária
+                Ahead(600);
+            }
+            else
+            {
+                Fire(1);
+                TurnLeft(evnt.Bearing);
+                Fire(1);
+                TurnLeft(90); //atira e se distancia para distancia contrária
+                Ahead(600);
+            }
+        }
+
+
+
+    }
+}
